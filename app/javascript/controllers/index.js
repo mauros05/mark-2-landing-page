a935d1359dcb5f1949e5a5e5d11da8b5edf9d2a5
Mauricio Gutierrez <emaurogtz@hotmail.es>
2022-09-04 19:38:35 -0500

    Add Stimulus to navbar

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,7 @@
-// Import and register all your controllers from the importmap under controllers/*
-
 import { application } from "controllers/application"
 
-// Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+import NavbarController from "./navbar_controller.js"
+application.register("navbar", NavbarController)
